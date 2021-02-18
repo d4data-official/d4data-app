c7db6354e60d57fdb97801913065c114bc2ecd20
Jessy SOBREIRO <jessy.sobreiro@gmail.com>
2021-02-18 02:28:52 +0100

    Add base work for contact/followings components

---
+++ followings/src/App.tsx
@@ -0,0 +1,135 @@
+import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import { withStyles, Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import Following from "./Following"
+import Typography from "@material-ui/core/Typography";
+import {Avatar} from "@material-ui/core";
+
+let jessy = {
+    'firstName': 'Jessy',
+    'lastName': 'SOBREIRO',
+    'email': 'jessy.sobreiro@epitech.eu',
+    'username': 'jess_sys',
+    'phoneNumber': '+33 6 59 28 15 37',
+    'birthday': '1998-11-17',
+    'nickname': 'M. Jessy SOBREIRO',
+    'creationDate': '2014-04-02',
+    'gender': 'Male',
+    'profilePicture': 'https://pbs.twimg.com/profile_images/888475149846106113/ddRQCpDE.jpg'
+}
+
+let clovis = {
+    'firstName': 'Clovis',
+    'lastName': 'ROUX DE VILLARS',
+    'email': 'clovis.roux-de-villars@epitech.eu',
+    'username': 'Xelorion',
+    'phoneNumber': '+33 6 69 69 69 69',
+    'birthday': '1989-04-21',
+    'nickname': 'M. Clovis De Villars',
+    'creationDate': '2012-09-02',
+    'gender': 'Male',
+    'profilePicture': ''
+}
+
+let profiles: { firstName: string; lastName: string; email: string; username: string; phoneNumber: string; birthday: string; nickname: string; creationDate: string; gender: string; profilePicture: string; }[] = [];
+
+profiles.push(jessy)
+profiles.push(clovis)
+
+const StyledTableCell = withStyles((theme: Theme) =>
+    createStyles({
+        head: {
+            backgroundColor: theme.palette.common.black,
+            color: theme.palette.common.white,
+        },
+        body: {
+            fontSize: 14,
+        },
+    }),
+)(TableCell);
+
+const StyledTableRow = withStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            '&:nth-of-type(odd)': {
+                backgroundColor: theme.palette.action.hover,
+            },
+        },
+    }),
+)(TableRow);
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 700,
+    },
+});
+
+function handleClick(setOpen: any, row: any, setProfile: any) {
+    setProfile(row)
+    setOpen(true);
+    return row;
+}
+
+export default function App() {
+    const classes = useStyles();
+    let [open, setOpen] = React.useState(false)
+    let [profile, setProfile] = React.useState({});
+
+    return (
+        <Container maxWidth="lg">
+            <Box my={4}>
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Contact list
+                </Typography>
+            </Box>
+            <Following state={[open, setOpen]} profile={profile}/>
+            <Box my={2}>
+                <TableContainer component={Paper}>
+                    <Table className={classes.table} size="small" aria-label="a dense table">
+                        <TableHead>
+                            <StyledTableRow>
+                                <StyledTableCell>Picture</StyledTableCell>
+                                <StyledTableCell>First name</StyledTableCell>
+                                <StyledTableCell>Last name</StyledTableCell>
+                                <StyledTableCell>Username</StyledTableCell>
+                                <StyledTableCell>E-mail</StyledTableCell>
+                            </StyledTableRow>
+                        </TableHead>
+                        <TableBody>
+                            {profiles.map((row) => (
+                                <StyledTableRow key={row.nickname}>
+                                    <TableCell onClick={() => handleClick(setOpen, row, setProfile)} component="th" scope="row">
+                                        <Avatar alt={row.firstName} src={row.profilePicture}>
+                                            {row.firstName.charAt(0)}
+                                            {row.lastName.charAt(0)}
+                                        </Avatar>
+                                    </TableCell>
+                                    <TableCell onClick={() => handleClick(setOpen, row, setProfile)} component="th" scope="row">
+                                        {row.firstName}
+                                    </TableCell>
+                                    <TableCell onClick={() => handleClick(setOpen, row, setProfile)} component="th" scope="row">
+                                        {row.lastName}
+                                    </TableCell>
+                                    <TableCell onClick={() => handleClick(setOpen, row, setProfile)} component="th" scope="row">
+                                        {row.username}
+                                    </TableCell>
+                                    <TableCell onClick={() => handleClick(setOpen, row, setProfile)} component="th" scope="row">
+                                        {row.email}
+                                    </TableCell>
+                                </StyledTableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Box>
+        </Container>
+    );
+}
---
+++ followings/src/Following.tsx
@@ -0,0 +1,199 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Container from "@material-ui/core/Container";
+import {Avatar, FormControlLabel, Grid} from "@material-ui/core";
+
+export default function Following(props: any): JSX.Element {
+    let states = props.state;
+    let profile = props.profile;
+    const [open, setOpen] = states;
+    const useStyles = makeStyles((theme: Theme) =>
+        createStyles({
+            root: {
+                display: 'flex',
+                '& > *': {
+                    margin: theme.spacing(1),
+                },
+            },
+            small: {
+                width: theme.spacing(3),
+                height: theme.spacing(3),
+            },
+            large: {
+                width: theme.spacing(16),
+                height: theme.spacing(16),
+            },
+            avatar: {
+                boxShadow: theme.shadows[3],
+                width: theme.spacing(16),
+                height: theme.spacing(16),
+            }
+        }),
+    );
+    const classes = useStyles();
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth={true} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Contact</DialogTitle>
+            <DialogContent>
+                <Grid container spacing={4}>
+                    <Grid item xs={5}/>
+                    <Grid item>
+                        <Avatar alt={profile.firstName} src={profile.profilePicture} className={classes.avatar}>
+                            {profile.firstName.charAt(0)}
+                            {profile.lastName.charAt(0)}
+                        </Avatar>
+                    </Grid>
+                </Grid>
+                <Grid container spacing={8}>
+                    <Grid item xs={1}/>
+                </Grid>
+                <Grid container spacing={4}>
+                    <Grid item xs={3}>
+                        <TextField
+                            id="outlined-read-only-input"
+                            label="Gender"
+                            defaultValue={profile.gender}
+                            fullWidth
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                            variant="outlined"
+                        />
+                    </Grid>
+                    <Grid item xs={1}/>
+                    <Grid item xs={4}>
+                        <TextField
+                            id="outlined-read-only-input"
+                            label="First name"
+                            defaultValue={profile.firstName}
+                            fullWidth
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                            variant="outlined"
+                        />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <TextField
+                            id="outlined-read-only-input"
+                            label="Last name"
+                            defaultValue={profile.lastName}
+                            fullWidth
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                            variant="outlined"
+                        />
+                    </Grid>
+                </Grid>
+                <Grid container spacing={4}>
+                    <Grid item xs={3}>
+                        <TextField
+                            id="date"
+                            label="Birthday"
+                            type="date"
+                            fullWidth
+                            defaultValue={profile.birthday}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                            variant="outlined"
+                        />
+                    </Grid>
+                    <Grid item xs={1}/>
+                    <Grid item xs={4}>
+                        <TextField
+                            id="outlined-read-only-input"
+                            label="Username"
+                            defaultValue={profile.username}
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                            fullWidth
+                            variant="outlined"
+                        />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <TextField
+                            id="outlined-read-only-input"
+                            label="Nickname"
+                            defaultValue={profile.nickname}
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                            fullWidth
+                            variant="outlined"
+                        />
+                    </Grid>
+                </Grid>
+                <Grid container spacing={4}>
+                    <Grid item xs={3}>
+                        <TextField
+                            id="date"
+                            label="Creation date"
+                            type="date"
+                            fullWidth
+                            defaultValue={profile.creationDate}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                            variant="outlined"
+                        />
+                    </Grid>
+                    <Grid item xs={1}/>
+                    <Grid item xs={4}>
+                        <TextField
+                            id="outlined-read-only-input"
+                            label="Email"
+                            defaultValue={profile.email}
+                            fullWidth
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                            variant="outlined"
+                        />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <TextField
+                            id="outlined-read-only-input"
+                            label="Phone"
+                            defaultValue={profile.phoneNumber}
+                            fullWidth
+                            InputProps={{
+                                readOnly: true,
+                            }}
+                            variant="outlined"
+                        />
+                    </Grid>
+
+                </Grid>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                    Close
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
