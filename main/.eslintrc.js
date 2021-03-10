e593b51abd6f1c5b8403a22f23cbafee374810ca
Maastrich <pinsaultm@gmail.com>
2021-03-10 19:34:18 +0100

    (config) update eslint, remove useless field in package.json

--- main/.eslintrc.js
+++ main/.eslintrc.js
@@ -2,5 +2,8 @@ module.exports = {
     extends: ['airbnb-typescript'],
     parserOptions: {
         project: './tsconfig.json',
+    },
+    rules: {
+        "import/no-extraneous-dependencies": "off"
     }
 };
