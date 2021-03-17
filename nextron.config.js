ac4bc7faf6e36c835e80ed23f7cf29a4c8361ab2
Maastrich <pinsaultm@gmail.com>
2021-03-17 13:14:46 +0100

    style(nextron.config): fix indentation

--- nextron.config.js
+++ nextron.config.js
@@ -3,23 +3,23 @@ const path = require('path');
 console.log(path.resolve(__dirname, 'main/helpers'))
 
 module.exports = {
-    // specify an alternate main src directory, defaults to 'main'
-    mainSrcDir: 'main',
-    // specify an alternate renderer src directory, defaults to 'renderer'
-    rendererSrcDir: 'renderer',
+	// specify an alternate main src directory, defaults to 'main'
+	mainSrcDir: 'main',
+	// specify an alternate renderer src directory, defaults to 'renderer'
+	rendererSrcDir: 'renderer',
 
-    // main process' webpack config
-    webpack: (defaultConfig, env) => {
-        // do some stuff here
-        return {
+	// main process' webpack config
+	webpack: (defaultConfig, env) => {
+		// do some stuff here
+		return {
 			...defaultConfig,
 			resolve: {
-                ...defaultConfig.resolve,
+				...defaultConfig.resolve,
 				alias: {
-                    ...defaultConfig.resolve?.alias,
+					...defaultConfig.resolve?.alias,
 					helpers: path.resolve(__dirname, 'main/helpers')
 				}
 			}
 		};
-    },
+	},
 };
