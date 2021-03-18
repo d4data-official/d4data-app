2743b1dd9f6f200aef7741730e6fb556f5ed6d27
Maastrich <pinsaultm@gmail.com>
2021-03-18 17:12:55 +0100

    build: changer package list and webpack-config

--- nextron.config.js
+++ nextron.config.js
@@ -1,25 +1,24 @@
 const path = require('path');
 
-console.log(path.resolve(__dirname, 'main/helpers'))
-
 module.exports = {
-	// specify an alternate main src directory, defaults to 'main'
-	mainSrcDir: 'main',
-	// specify an alternate renderer src directory, defaults to 'renderer'
-	rendererSrcDir: 'renderer',
+  // specify an alternate main src directory, defaults to 'main'
+  mainSrcDir: 'main',
+  // specify an alternate renderer src directory, defaults to 'renderer'
+  rendererSrcDir: 'renderer',
 
-	// main process' webpack config
-	webpack: (defaultConfig, env) => {
-		// do some stuff here
-		return {
-			...defaultConfig,
-			resolve: {
-				...defaultConfig.resolve,
-				alias: {
-					...defaultConfig.resolve?.alias,
-					helpers: path.resolve(__dirname, 'main/helpers')
-				}
-			}
-		};
-	},
+  // main process' webpack config
+  webpack: (defaultConfig, env) => {
+    // do some stuff here
+    return {
+      ...defaultConfig,
+      resolve: {
+        ...defaultConfig.resolve,
+        alias: {
+          ...defaultConfig.resolve?.alias,
+          helpers: path.resolve(__dirname, 'main/helpers'),
+          "ipcmain": path.resolve(__dirname, 'main/utils/ipc')
+        }
+      }
+    };
+  },
 };
