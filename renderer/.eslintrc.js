d38ccfc880413b1cad394cccee788bb2485678dd
Maastrich <pinsaultm@gmail.com>
2021-03-19 14:18:35 +0100

    style: change eslint config and patch 4spaces tabulations

--- renderer/.eslintrc.js
+++ renderer/.eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-    extends: ['airbnb-typescript'],
-    parserOptions: {
-        project: './tsconfig.json',
-    },
-    rules: {
-        "react/jsx-props-no-spreading": "off",
-        "jsx-a11y/anchor-has-content": "off",
-        "react/require-default-props": "off",
-        "import/no-extraneous-dependencies": [2, { devDependencies: true }]
-    }
+  extends: ['airbnb-typescript'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    "react/jsx-props-no-spreading": "off",
+    "jsx-a11y/anchor-has-content": "off",
+    "react/require-default-props": "off",
+    "import/no-extraneous-dependencies": [2, { devDependencies: true }]
+  }
 };
