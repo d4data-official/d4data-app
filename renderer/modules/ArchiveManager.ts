a1b2e36b112f0399c4e3fe5fc7fb0e5beb6405cb
Clovis de Villars <clovis.devillars@gmail.com>
2021-04-28 12:51:30 +0200

    feat(ArchiveManger): add class to manage current archive

---
+++ renderer/modules/ArchiveManager.ts
@@ -0,0 +1,7 @@
+import StandardizerIPC from '@shared/d4data-archive-lib/renderer/StandardizerIPC'
+
+class ArchiveManager {
+  currentStandardizer?: StandardizerIPC
+}
+
+export default new ArchiveManager()
