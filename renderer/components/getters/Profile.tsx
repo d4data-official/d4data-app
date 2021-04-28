9fe8543f81ea8e46883785de69d4cc4b36e111c5
Clovis de Villars <clovis.devillars@gmail.com>
2021-04-28 17:43:42 +0200

    refactor(profile-page): refactor props type

--- renderer/components/getters/Profile.tsx
+++ renderer/components/getters/Profile.tsx
@@ -1,7 +1,11 @@
 import type { GetterData } from 'd4data-archive-lib/dist/src/types/standardizer/GetterReturn'
 import type { Profile as ProfileType } from 'd4data-archive-lib/dist/src/types/schemas'
 
-export default function Profile({ data }: { data: GetterData<Array<ProfileType>> }) {
+export interface Props {
+  data: NonNullable<GetterData<Array<ProfileType>>>
+}
+
+export default function Profile({ data }: Props) {
   return (
     <div>
       <div>Profile</div>
