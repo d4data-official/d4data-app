5498cbeb454419a7a1d16518c0b2e40c50635a4d
Clovis de Villars <clovis.devillars@gmail.com>
2021-05-05 19:34:50 +0200

    style(eslint): update rules for main process code
    Allow console.[info,warn,error]

--- main/.eslintrc.js
+++ main/.eslintrc.js
@@ -8,5 +8,13 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'warn',
     "template-curly-spacing": ["error", "always"],
     'import/no-extraneous-dependencies': 'off',
+    'no-console': 'off',
+    'no-restricted-syntax': [
+      'warn',
+      {
+        'selector': 'CallExpression[callee.object.name=\'console\'][callee.property.name!=/^(info|warn|error)$/]',
+        'message': 'Unexpected console log. If you want to print message/warning/error during run time use console.[info|warn|error].',
+      },
+    ],
   },
 }
