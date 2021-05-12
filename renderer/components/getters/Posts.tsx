9019d83c2e4cb203cc2e8ab255473a9bd1b819f1
Jessy SOBREIRO <jessy.sobreiro@gmail.com>
2021-05-12 02:05:24 +0200

    Fix dist/src

--- renderer/components/getters/Posts.tsx
+++ renderer/components/getters/Posts.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { GetterData } from '@d4data/archive-lib/dist/src/types/standardizer/GetterReturn'
-import { Post } from '@d4data/archive-lib/src/types/schemas'
+import { Post } from '@d4data/archive-lib/dist/src/types/schemas'
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
