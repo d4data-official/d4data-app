13e90e35d1793e51d8e4a6b71c19223078bf7c97
Jessy SOBREIRO <jessy.sobreiro@epitech.eu>
2021-05-13 00:30:44 +0200

    Fix loadLimit

--- renderer/components/getters/Posts.tsx
+++ renderer/components/getters/Posts.tsx
@@ -6,16 +6,18 @@ import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import PostComponent from './PostComponents/PostComponent'
 
+const loadLimit = 47
+
 export default function Posts({ data }: { data: NonNullable<GetterData<Array<Post>>> }) {
   return (
     <Container maxWidth="md">
       <Box my={ 4 }>
         <Typography variant="h5" gutterBottom>
-          { `${ data.data.length } posts found` }
+          { `${ data.data.slice(0, loadLimit).length } posts found` }
         </Typography>
       </Box>
       <Box my={ 2 }>
-        {data.data.map((row, idx) => (
+        {data.data.slice(0, loadLimit).map((row, idx) => (
           <div>
             <PostComponent key={ idx.toString() } data={ row } />
             <br/>
