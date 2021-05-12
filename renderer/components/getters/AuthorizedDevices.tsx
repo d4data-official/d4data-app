da8984b82d0a7d55b5bd83c9540fac827d6da3fb
Jessy SOBREIRO <jessy.sobreiro@gmail.com>
2021-05-12 23:24:10 +0200

    Update AuthorizedDevices.tsx

--- renderer/components/getters/AuthorizedDevices.tsx
+++ renderer/components/getters/AuthorizedDevices.tsx
@@ -82,7 +82,7 @@ export default function AuthorizedDevices({ data }: { data: NonNullable<GetterDa
                       component="th"
                       scope="row"
                     >
-                      { device.authorizationDate.toLocaleString() }
+                      { device.authorizationDate?.toLocaleString() ?? 'No date provided' }
                     </TableCell>
                   </StyledTableRow>
                 )
