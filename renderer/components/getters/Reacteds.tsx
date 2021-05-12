d72350702db689fcc8231b9ec8ee6079560a77c1
Jessy SOBREIRO <jessy.sobreiro@epitech.eu>
2021-05-12 20:21:30 +0200

    Add loadLimit

--- renderer/components/getters/Reacteds.tsx
+++ renderer/components/getters/Reacteds.tsx
@@ -9,16 +9,18 @@ import ReactedMediaComponent from './ReactedComponents/ReactedMediaComponent';
 import ReactedCommunityComponent from './ReactedComponents/ReactedCommunityComponent';
 import ReactedLinkComponent from './ReactedComponents/ReactedLinkComponent';
 
+const loadLimit = 114
+
 export default function Reacteds({ data }: { data: NonNullable<GetterData<Array<Reacted>>> }) {
   return (
     <Container maxWidth="md">
       <Box my={ 4 }>
         <Typography variant="h5" gutterBottom>
-          { `${ data.data.length } reactions found` }
+          { `${ data.data.slice(0, loadLimit).length } reactions found` }
         </Typography>
       </Box>
       <Box my={ 2 }>
-        {data.data.slice(0, 100).map((row, idx) => (
+        {data.data.slice(0, loadLimit).map((row, idx) => (
           <div>
             { row.entityType === 'post' && <ReactedPostComponent key={ idx.toString() } data={ row } />}
             { row.entityType === 'community' && <ReactedCommunityComponent key={ idx.toString() } data={ row } />}
