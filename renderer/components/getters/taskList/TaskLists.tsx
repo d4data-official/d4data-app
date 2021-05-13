5aa71a031e3706bb6fe82793902513e80de72097
Jessy SOBREIRO <jessy.sobreiro@epitech.eu>
2021-05-13 17:12:32 +0200

    Fix dist/src

--- renderer/components/getters/taskList/TaskLists.tsx
+++ renderer/components/getters/taskList/TaskLists.tsx
@@ -14,8 +14,8 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import clsx from 'clsx';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import type { TaskList } from '@d4data/archive-lib/src/types/schemas';
-import type { Task } from '@d4data/archive-lib/src/types/schemas/TaskList'
+import type { TaskList } from '@d4data/archive-lib/dist/src/types/schemas';
+import type { Task } from '@d4data/archive-lib/dist/src/types/schemas/TaskList'
 import { GetterData } from '@d4data/archive-lib/dist/src/types/standardizer/GetterReturn';
 
 const drawerWidth = 240;
