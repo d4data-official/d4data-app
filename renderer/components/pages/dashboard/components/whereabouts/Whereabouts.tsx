4a3988ff8ef6090196f3510752e6f761f020b765
Jessy SOBREIRO <jessy.sobreiro@epitech.eu>
2021-05-13 16:17:25 +0200

    Fix whereabouts
    You need to load the table first to get the markers on the map

--- renderer/components/pages/dashboard/components/whereabouts/Whereabouts.tsx
+++ renderer/components/pages/dashboard/components/whereabouts/Whereabouts.tsx
@@ -2,61 +2,41 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Table from './components/Table'
 import {
   AppBar, Container, Typography,
 } from '@material-ui/core';
-import { withStyles, Theme, createStyles, makeStyles } from '@material-ui/core/styles';
-
-import Map from './components/Map'
-import MUIDataTable from 'mui-datatables';
+import { makeStyles } from '@material-ui/core/styles';
 import MapIcon from '@material-ui/icons/Map';
 import ListIcon from '@material-ui/icons/List';
 import type { Whereabout } from '@d4data/archive-lib/dist/src/types/schemas';
+import Table from './components/Table'
+import Map from './components/Map'
+
+const E7 = 10000000
 
 async function resolveLocations(whereabouts: Array<Whereabout>) {
-  console.log(whereabouts)
-  const result = whereabouts?.map(async (whereabout: Whereabout) => {
-    console.log('debut')
-    console.log(whereabout)
-
-    if (!whereabout.location.relativePosition && !whereabout.location.absolutePosition) {
-      console.log('la')
+  const result = whereabouts.map(async (whereabout: Whereabout) => {
+    const whereaboutCopy = { ...whereabout }
+    if (whereaboutCopy.location?.absolutePosition) {
+      whereaboutCopy.location.absolutePosition.latitude = whereaboutCopy.location?.absolutePosition?.latitude / E7
+          ?? 0
+      whereaboutCopy.location.absolutePosition.longitude = whereaboutCopy.location?.absolutePosition?.longitude / E7
+          ?? 0
+    }
+    if (!whereabout.location?.relativePosition && !whereabout.location?.absolutePosition) {
       return whereabout
     }
-    if (!whereabout.location.absolutePosition) {
+    if (!whereabout.location?.absolutePosition) {
       const address = whereabout?.location?.relativePosition?.raw
-      // eslint-disable-next-line max-len
-      const res = await axios.get(`https://nominatim.openstreetmap.org/?format=json&addressdetails=1&q=${ address }&format=json&limit=1`)
-      console.log('ici')
-      // eslint-disable-next-line no-param-reassign
-      whereabout.location.absolutePosition = { latitude: 0, longitude: 0 }
-      // eslint-disable-next-line no-param-reassign
-      whereabout.location.absolutePosition.latitude = res?.data?.[0]?.lat
-      // eslint-disable-next-line no-param-reassign
-      whereabout.location.absolutePosition.longitude = res?.data?.[0]?.lon
+      const res = await axios.get(
+        `https://nominatim.openstreetmap.org/?format=json&addressdetails=1&q=${ address }&format=json&limit=1`,
+      )
+      whereaboutCopy.location.absolutePosition = { latitude: 0, longitude: 0 }
+      whereaboutCopy.location.absolutePosition.latitude = res?.data?.[0]?.lat
+      whereaboutCopy.location.absolutePosition.longitude = res?.data?.[0]?.lon
     }
-    if (!whereabout.location.relativePosition) {
-      console.log('tf')
-      const lat = whereabout?.location?.absolutePosition?.latitude
-      const lon = whereabout?.location?.absolutePosition?.longitude
-      // eslint-disable-next-line max-len
-      const res = await axios.get(`https://nominatim.openstreetmap.org/reverse?format=json&addressdetails=1&zoom=18&lat=${ lat }&lon=${ lon }`)
-      // eslint-disable-next-line no-param-reassign
-      whereabout.location.relativePosition = {
-        address: undefined,
-        city: undefined,
-        country: undefined,
-        raw: '',
-        zipcode: undefined,
-      }
-      // eslint-disable-next-line no-param-reassign
-      whereabout.location.relativePosition.raw = res.data.display_name
-    }
-    console.log('final')
-    console.log(whereabout)
-    console.log('------------')
-    return whereabout
+    console.log(whereaboutCopy)
+    return whereaboutCopy
   })
   return Promise.all(result)
 }
@@ -140,9 +120,8 @@ interface Prop {
 function Whereabouts({ data }: { data: Prop }) {
   const [data2, setData] = useState<any>([])
   useEffect(() => {
-    console.log(data)
-    setData(data?.data)
-    resolveLocations(data?.data ?? []).then((locations) => setData(locations))
+    setData(data.data?.slice(0, 1))
+    resolveLocations(data.data?.slice(0, 1) ?? []).then((locations) => setData(locations))
   }, [data.data])
   return (
     <>
--- renderer/components/pages/dashboard/components/whereabouts/components/Marker.tsx
+++ renderer/components/pages/dashboard/components/whereabouts/components/Marker.tsx
@@ -5,28 +5,28 @@ async function imports2({ whereabouts }: { whereabouts: Array<Whereabout> }) {
   const {
     Marker, Popup,
   } = await import('react-leaflet')
-
   return (
     <>
-      {whereabouts?.filter((location: any) => location.location.absolutePosition).map((location: any, i: Number) => {
-        const position = location.location.absolutePosition
-        return (
-          <Marker key={ i.toString() } position={ [position.latitude, position.longitude] }>
-            <Popup>
-              <b>{location?.location?.relativePosition?.raw ?? ''}</b>
-              <br/>
-              {location.recordDate.toString()}
-              {' '}
-              <br/>
-              {position.latitude}
-              ,
-              {position.longitude}
-              {' '}
-              <br/>
-            </Popup>
-          </Marker>
-        )
-      })}
+      {whereabouts?.filter((location: Whereabout) => location.location?.absolutePosition !== undefined)
+        .map((location: Whereabout, i: Number) => {
+          const position = location.location.absolutePosition
+          return (
+            <Marker key={ i.toString() } position={ [position.latitude, position.longitude] }>
+              <Popup>
+                <b>{location?.location?.relativePosition?.raw ?? ''}</b>
+                <br/>
+                {location.recordDate?.toLocaleString() ?? 'No date provided'}
+                {' '}
+                <br/>
+                {position.latitude }
+                ,
+                {position.longitude }
+                {' '}
+                <br/>
+              </Popup>
+            </Marker>
+          )
+        })}
     </>
   )
 }
