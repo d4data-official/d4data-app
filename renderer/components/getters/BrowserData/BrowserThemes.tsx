c685e669f30c1115bf76720f7d2f49e155755f40
Clovis de Villars <clovis.devillars@gmail.com>
2021-05-15 20:38:29 +0200

    feat(BrowserThemes): implement browser theme display

--- renderer/components/getters/BrowserData/BrowserThemes.tsx
+++ renderer/components/getters/BrowserData/BrowserThemes.tsx
@@ -1,5 +1,56 @@
-export default function BrowserThemes() {
+import { Theme } from '@d4data/archive-lib/dist/src/types/schemas/BrowserData'
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Container,
+  Tooltip,
+  Typography,
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import openInBrowser from '../../../modules/openInBrowser'
+
+export interface Props {
+  data: Theme
+}
+
+const useStyles = makeStyles({
+  root: {
+    height: '100%',
+  },
+})
+
+export default function BrowserThemes({ data }: Props) {
+  const classes = useStyles()
+
   return (
-    <div>BrowserThemes</div>
+    <Container maxWidth="sm" className={ classes.root }>
+      <Box height={ 1 } width={ 1 } display="flex" flexDirection="column" justifyContent="center">
+        <Card>
+          <CardHeader title="Current browser theme"/>
+          <CardContent>
+            <Box display="flex" alignItems="center" justifyContent="center">
+              <Typography color="primary" variant="h6">{ data.name }</Typography>
+            </Box>
+          </CardContent>
+          { data.websiteUrl && (
+            <CardActions>
+              <Tooltip title={ data.websiteUrl } placement="bottom">
+                <Button
+                  onClick={ () => openInBrowser(data.websiteUrl!) }
+                  variant="outlined"
+                  color="primary"
+                  fullWidth
+                >Open website
+                </Button>
+              </Tooltip>
+            </CardActions>
+          ) }
+        </Card>
+      </Box>
+    </Container>
   )
 }
