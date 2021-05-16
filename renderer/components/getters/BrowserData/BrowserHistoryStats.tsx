56fbdda19e85f594211acf0bf35821a049209329
Clovis de Villars <clovis.devillars@gmail.com>
2021-05-16 21:04:07 +0200

    feat(BrowserData-page): add some stat cards for browser history

--- renderer/components/getters/BrowserData/BrowserHistoryStats.tsx
+++ renderer/components/getters/BrowserData/BrowserHistoryStats.tsx
@@ -1,5 +1,37 @@
-export default function BrowserHistoryStats() {
+import { Grid } from '@material-ui/core'
+import { createStyles, makeStyles } from '@material-ui/core/styles'
+import { useMemo } from 'react'
+import { BrowserData } from '@d4data/archive-lib'
+import HistoryCountCard from './BrowserHistoryStats/HistoryCountCard'
+import BrowserDataStats from '../../../modules/stats/BrowserDataStats'
+import HistoryWebsiteCountCard from './BrowserHistoryStats/HistoryWebsiteCountCard'
+import HistoryDurationCard from './BrowserHistoryStats/HistoryDurationCard'
+
+export interface Props {
+  data: BrowserData
+}
+
+const useStyles = makeStyles((theme) => createStyles({
+  root: {
+    padding: theme.spacing(0),
+  },
+}))
+
+export default function BrowserHistoryStats({ data }: Props) {
+  const classes = useStyles()
+  const browserDataStats = useMemo(() => new BrowserDataStats(data), [data])
+
   return (
-    <div>BrowserHistoryStats</div>
+    <Grid container spacing={ 4 } className={ classes.root }>
+      <Grid item>
+        <HistoryCountCard browserDataStats={ browserDataStats }/>
+      </Grid>
+      <Grid item>
+        <HistoryWebsiteCountCard browserDataStats={ browserDataStats }/>
+      </Grid>
+      <Grid item>
+        <HistoryDurationCard browserDataStats={ browserDataStats }/>
+      </Grid>
+    </Grid>
   )
 }
