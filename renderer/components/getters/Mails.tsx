735cc45efa6487dbc38f2c2e75f95dee888475d7
Nathan Bonnot <nathan.bonnot@epitech.eu>
2021-05-17 12:57:22 +0200

    Fixed lib name

--- renderer/components/getters/Mails.tsx
+++ renderer/components/getters/Mails.tsx
@@ -4,8 +4,8 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { Box, Divider, Grid } from '@material-ui/core';
-import type { GetterData } from 'd4data-archive-lib/dist/src/types/standardizer/GetterReturn'
-import type { Mail as MailsType } from 'd4data-archive-lib/dist/src/types/schemas'
+import type { GetterData } from '@d4data/archive-lib/dist/src/types/standardizer/GetterReturn'
+import type { Mail as MailsType } from '@d4data/archive-lib/dist/src/types/schemas'
 
 const useStyles = makeStyles({
   root: {
@@ -35,24 +35,24 @@ export default function Mails({ data }: Props) {
   const classes = useStyles();
 
   return (
-    <Box width={1} height={1}>
+    <Box width={ 1 } height={ 1 }>
       <h2>{data.data.length} Mails</h2>
-      <Grid container spacing={2}>
+      <Grid container spacing={ 2 }>
         {
           data.data.map((mail) => (
-            <Grid item xs={12}>
-              < Card className={classes.root} >
+            <Grid item xs={ 12 }>
+              < Card className={ classes.root } >
                 <CardContent>
-                  <Typography variant="h5" className={classes.title} gutterBottom>
+                  <Typography variant="h5" className={ classes.title } gutterBottom>
                     {mail.subject}
                   </Typography>
-                  <Typography variant="h5" className={classes.subtitle} gutterBottom>
+                  <Typography variant="h5" className={ classes.subtitle } gutterBottom>
                     From : {mail.from}
                   </Typography>
-                  <Typography variant="h5" className={classes.subtitle} gutterBottom>
+                  <Typography variant="h5" className={ classes.subtitle } gutterBottom>
                     To : {mail.to.join(', ')}
                   </Typography>
-                  <Typography variant="h5" className={classes.subtitle} gutterBottom>
+                  <Typography variant="h5" className={ classes.subtitle } gutterBottom>
                     Date : {mail.date.toLocaleString()}
                   </Typography>
                   <Typography variant="body1" component="p">
