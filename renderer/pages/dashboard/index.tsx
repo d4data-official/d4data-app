6cbc72618eb798543a9b10f65729fafe957d4b44
Clovis de Villars <clovis.devillars@gmail.com>
2021-05-17 21:01:37 +0200

    fix(Dashboard): handle restored archives

--- renderer/pages/dashboard/index.tsx
+++ renderer/pages/dashboard/index.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import Filesize from 'filesize';
-import { ArchiveMetaData } from '@d4data/archive-lib';
-import { Card, Container, Grid, Typography } from '@material-ui/core';
-import Rating from '@material-ui/lab/Rating';
-import UnarchiveIcon from '@material-ui/icons/Unarchive';
-import Facebook from '@material-ui/icons/Facebook';
-import Reddit from '@material-ui/icons/Reddit';
-import EventNoteIcon from '@material-ui/icons/EventNote';
-import moment from 'moment';
-import ArchiveManager from '../../modules/ArchiveManager';
-import MetadataCard from '../../components/pages/dashboard/components/MetaDataCard';
+import Filesize from 'filesize'
+import { ArchiveMetaData } from '@d4data/archive-lib'
+import { Card, Container, Grid, Typography } from '@material-ui/core'
+import Rating from '@material-ui/lab/Rating'
+import UnarchiveIcon from '@material-ui/icons/Unarchive'
+import Facebook from '@material-ui/icons/Facebook'
+import Reddit from '@material-ui/icons/Reddit'
+import EventNoteIcon from '@material-ui/icons/EventNote'
+import moment from 'moment'
+import MetadataCard from '../../components/pages/dashboard/components/MetaDataCard'
+import useArchiveManager from '../../hooks/useArchiveManager'
 
 function computeLogoFromServiceName(serviceName: string) {
   if (serviceName.toUpperCase() === 'FACEBOOK') return (<Facebook/>)
@@ -25,13 +25,13 @@ function generateTeamNotes(serviceName: string) {
       <h2>A note from the team</h2>
 
       <Card component="fieldset" style={ { borderColor: 'transparent', padding: 15, paddingBottom: 5 } }>
-        <Grid container >
-          <Grid item >
+        <Grid container>
+          <Grid item>
             <Typography component="legend">GDPR Score (coming soon)</Typography>
-            <Rating name="read-only" value={ 0 } disabled readOnly />
+            <Rating name="read-only" value={ 0 } disabled readOnly/>
           </Grid>
         </Grid>
-        <Grid item >
+        <Grid item>
           <p>This section will help you know more about the services you are using and their data policies.</p>
           <p>D4Data will set a score for each supported services. More to come during the release!</p>
         </Grid>
@@ -42,12 +42,12 @@ function generateTeamNotes(serviceName: string) {
 }
 
 const Dashboard = () => {
-  const [archiveMetadata, setArchiveMetadata] = useState<ArchiveMetaData>()
+  const [archiveMetadata, setArchiveMetadata] = useState<ArchiveMetaData | undefined>()
+  const { getArchiveMetadata } = useArchiveManager()
 
   useEffect(() => {
-    ArchiveManager.currentArchive?.getMetadata().then((metadata) => {
-      setArchiveMetadata(metadata)
-    })
+    getArchiveMetadata()
+      .then((metadata) => setArchiveMetadata(metadata))
   }, [])
 
   return (
@@ -64,22 +64,26 @@ const Dashboard = () => {
           />
 
         </Grid>
-        <Grid item xs={ 4 }>
-          <MetadataCard
-            icon={ <UnarchiveIcon/> }
-            metadata={ archiveMetadata?.size ? Filesize(archiveMetadata?.size) : 'Loading...' }
-            subtitle="Archive size"
-          />
-        </Grid>
-        <Grid item xs={ 4 }>
-          <MetadataCard
-            icon={ <EventNoteIcon/> }
-            metadata={ archiveMetadata?.creationDate
-              ? moment.duration(archiveMetadata?.creationDate?.valueOf() / 100).humanize()
-              : 'Loading...' }
-            subtitle="Archive age"
-          />
-        </Grid>
+
+        { archiveMetadata?.size && (
+          <Grid item xs={ 4 }>
+            <MetadataCard
+              icon={ <UnarchiveIcon/> }
+              metadata={ Filesize(archiveMetadata?.size) }
+              subtitle="Archive size"
+            />
+          </Grid>
+        ) }
+
+        { archiveMetadata?.creationDate && (
+          <Grid item xs={ 4 }>
+            <MetadataCard
+              icon={ <EventNoteIcon/> }
+              metadata={ moment.duration(archiveMetadata?.creationDate?.valueOf() / 100).humanize() }
+              subtitle="Archive age"
+            />
+          </Grid>
+        ) }
       </Grid>
       <br/>
       <div>
