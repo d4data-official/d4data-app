4e2aad269a3a5003edda2f5b563711aa13f1f2c4
Nathan Bonnot <nathan.bonnot@epitech.eu>
2021-05-17 12:52:23 +0200

    Fixed html

--- renderer/components/getters/Mails.tsx
+++ renderer/components/getters/Mails.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import type { Mail as MailsType } from 'd4data-archive-lib/dist/src/types/schemas'
-import type { GetterData } from 'd4data-archive-lib/dist/src/types/standardizer/GetterReturn'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import { Divider } from '@material-ui/core';
+import { Box, Divider, Grid } from '@material-ui/core';
+import type { GetterData } from 'd4data-archive-lib/dist/src/types/standardizer/GetterReturn'
+import type { Mail as MailsType } from 'd4data-archive-lib/dist/src/types/schemas'
 
 const useStyles = makeStyles({
   root: {
-    minWidth: 275,
+    width: '100%',
   },
   bullet: {
     display: 'inline-block',
@@ -31,37 +31,40 @@ export interface Props {
   data: NonNullable<GetterData<Array<MailsType>>>
 }
 
-
 export default function Mails({ data }: Props) {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
 
   return (
-    <>
-      {
-        data.data.map((mail) => (
-          < Card className={classes.root} >
-            <CardContent>
-              <Typography variant="h5" className={classes.title} gutterBottom>
-                Object: {mail.subject}
-              </Typography>
-              <Typography variant="h5" className={classes.subtitle} gutterBottom>
-                From :{mail.from}
-              </Typography>
-              <Typography variant="h5" className={classes.subtitle} gutterBottom>
-                To :{mail.to}
-              </Typography>
-              <Typography variant="body2" component="h2">
-                Date: {mail.date.toLocaleString()}
-              </Typography>
-              <Typography variant="body1" component="p">
-                {mail.content}
-              </Typography>
-            </CardContent>
-            <Divider />
-          </Card >
-        ))
-      }
-    </>
+    <Box width={1} height={1}>
+      <h2>{data.data.length} Mails</h2>
+      <Grid container spacing={2}>
+        {
+          data.data.map((mail) => (
+            <Grid item xs={12}>
+              < Card className={classes.root} >
+                <CardContent>
+                  <Typography variant="h5" className={classes.title} gutterBottom>
+                    {mail.subject}
+                  </Typography>
+                  <Typography variant="h5" className={classes.subtitle} gutterBottom>
+                    From : {mail.from}
+                  </Typography>
+                  <Typography variant="h5" className={classes.subtitle} gutterBottom>
+                    To : {mail.to.join(', ')}
+                  </Typography>
+                  <Typography variant="h5" className={classes.subtitle} gutterBottom>
+                    Date : {mail.date.toLocaleString()}
+                  </Typography>
+                  <Typography variant="body1" component="p">
+                    {mail.content}
+                  </Typography>
+                </CardContent>
+                <Divider />
+              </Card >
+            </Grid>
+          ))
+        }
+      </Grid>
+    </Box>
   );
 }
