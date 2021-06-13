af2233e0d4c6834f939bff4d6b57cb99721e8e55
Clovis de Villars <clovis.devillars@gmail.com>
2021-06-13 16:53:51 +0200

    fix(material-ui): fix theme

--- renderer/themeDark.ts
+++ renderer/themeDark.ts
@@ -1,9 +1,8 @@
-import { createMuiTheme } from '@material-ui/core/styles'
+import { createTheme } from '@material-ui/core/styles'
 import red from '@material-ui/core/colors/red'
 
-export default createMuiTheme({
+export default createTheme({
   palette: {
-    type: 'dark',
     primary: {
       // main: '#556cd6', // Blue
       main: '#00c687', // Green
