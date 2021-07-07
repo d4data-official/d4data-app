6cca05807f5e8505886cd38b06b6aef598ff88d7
Clovis de Villars <clovis.devillars@gmail.com>
2021-07-07 15:49:15 +0200

    fix(theme): fix color import

--- renderer/themeDark.ts
+++ renderer/themeDark.ts
@@ -1,5 +1,5 @@
 import { createTheme } from '@material-ui/core/styles'
-import red from '@material-ui/core/colors/red'
+import { red } from '@material-ui/core/colors'
 
 export default createTheme({
   palette: {
