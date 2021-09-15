c787059f983e1b23d60fda321ae1bbcb878f887d
Clovis de Villars <clovis.devillars@gmail.com>
2021-09-15 18:11:03 +0200

    feat(components): add StatisticPage component

---
+++ renderer/components/statistics/StatisticPage.tsx
@@ -0,0 +1,25 @@
+import Statistic from '@d4data/archive-lib/dist/src/types/schemas/Statistic'
+import { Box, Grid } from '@material-ui/core'
+import React from 'react'
+import StatisticCard, { Props as StatisticCardProps } from './StatisticCard'
+
+export interface Props {
+  statistics: Array<Statistic>
+  variantProvider?: (statistic: Statistic, index: number) => StatisticCardProps['variant']
+}
+
+export default function StatisticPage({ statistics, variantProvider }: Props) {
+  return (
+    <Box height={ 1 } padding={ 4 } overflow="auto">
+      <Grid container spacing={ 4 } justifyContent="center">
+        {
+          statistics.map((statistic, index) => (
+            <Grid item>
+              <StatisticCard statistic={ statistic } variant={ variantProvider?.(statistic, index) }/>
+            </Grid>
+          ))
+        }
+      </Grid>
+    </Box>
+  )
+}
