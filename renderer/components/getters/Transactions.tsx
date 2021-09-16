037beb031a3364ad0dbb67025b9357ed932c6d64
Clovis de Villars <clovis.devillars@gmail.com>
2021-09-16 02:11:20 +0200

    feat(Transactions): add statistics

--- renderer/components/getters/Transactions.tsx
+++ renderer/components/getters/Transactions.tsx
@@ -1,5 +1,5 @@
 import { Transaction } from '@d4data/archive-lib/dist/src/types/schemas'
-import React from 'react'
+import React, { useState } from 'react'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import { makeStyles } from '@material-ui/styles'
@@ -11,7 +11,12 @@ import Paper from '@material-ui/core/Paper'
 import Box from '@material-ui/core/Box'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
+import { Tab, Tabs } from '@material-ui/core'
+import { History, Timeline } from '@material-ui/icons'
+import { grey } from '@material-ui/core/colors'
 import type { GetterData } from '@d4data/archive-lib/dist/src/types/standardizer/GetterReturn'
+import AutoStatisticPage from '../statistics/AutoStatisticPage'
+import Getters from '@d4data/archive-lib/dist/src/types/standardizer/Getters'
 
 export interface Props {
   data: NonNullable<GetterData<Array<Transaction>>>
@@ -25,68 +30,104 @@ const useStyles = makeStyles({
 
 export default function Transactions({ data }: { data: NonNullable<GetterData<Array<Transaction>>> }) {
   const classes = useStyles()
+  const [currentTab, setCurrentTab] = useState(0)
+
+  const getTabContent = () => {
+    switch (currentTab) {
+      case 0:
+        return <AutoStatisticPage getter={ Getters.TRANSACTIONS }/>
+      case 1:
+        return (
+          <Container maxWidth="lg">
+            <Box my={ 4 }>
+              <Typography variant="h5" gutterBottom>
+                { `${ data.data.length } transactions found` }
+              </Typography>
+            </Box>
+            <Box my={ 2 }>
+              <TableContainer component={ Paper }>
+                <Table className={ classes.table } size="small" aria-label="a dense table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Item</TableCell>
+                      <TableCell>Price (Currency)</TableCell>
+                      <TableCell>Payment method</TableCell>
+                      <TableCell>Transaction status</TableCell>
+                      <TableCell>Transaction date</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    { data.data.map((row, idx) => {
+                      const transaction = row
+                      return (
+                        <TableRow key={ idx.toString() }>
+                          <TableCell
+                            component="th"
+                            scope="row"
+                          >
+                            { transaction.product }
+                          </TableCell>
+                          <TableCell
+                            component="th"
+                            scope="row"
+                          >
+                            { `${ transaction.value } (${ transaction.currency })` }
+                          </TableCell>
+                          <TableCell
+                            component="th"
+                            scope="row"
+                          >
+                            { transaction.paymentMethod }
+                          </TableCell>
+                          <TableCell
+                            component="th"
+                            scope="row"
+                          >
+                            { transaction.status }
+                          </TableCell>
+                          <TableCell
+                            component="th"
+                            scope="row"
+                          >
+                            { transaction.date?.toLocaleString() ?? 'No date provided' }
+                          </TableCell>
+                        </TableRow>
+                      )
+                    }) }
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Box>
+          </Container>
+        )
+      default:
+        return undefined
+    }
+  }
 
   return (
-    <Container maxWidth="lg">
-      <Box my={ 4 }>
-        <Typography variant="h5" gutterBottom>
-          { `${ data.data.length } transactions found` }
-        </Typography>
+    <Box width={ 1 } flexGrow={ 1 } display="flex" flexDirection="column">
+      <Box padding={ 0 } paddingBottom={ 0 }>
+        <Tabs
+          value={ currentTab }
+          onChange={ (event, newValue) => setCurrentTab(newValue) }
+          variant="fullWidth"
+          indicatorColor="primary"
+          textColor="primary"
+          centered
+          sx={ {
+            minHeight: 72,
+            '& .Mui-selected': {
+              background: grey['50'],
+            },
+          } }
+        >
+          <Tab icon={ <Timeline/> } label="History stat"/>
+          <Tab icon={ <History/> } label="Transactions"/>
+        </Tabs>
       </Box>
-      <Box my={ 2 }>
-        <TableContainer component={ Paper }>
-          <Table className={ classes.table } size="small" aria-label="a dense table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Item</TableCell>
-                <TableCell>Price (Currency)</TableCell>
-                <TableCell>Payment method</TableCell>
-                <TableCell>Transaction status</TableCell>
-                <TableCell>Transaction date</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              { data.data.map((row, idx) => {
-                const transaction = row
-                return (
-                  <TableRow key={ idx.toString() }>
-                    <TableCell
-                      component="th"
-                      scope="row"
-                    >
-                      { transaction.product }
-                    </TableCell>
-                    <TableCell
-                      component="th"
-                      scope="row"
-                    >
-                      { `${ transaction.value } (${ transaction.currency })` }
-                    </TableCell>
-                    <TableCell
-                      component="th"
-                      scope="row"
-                    >
-                      { transaction.paymentMethod }
-                    </TableCell>
-                    <TableCell
-                      component="th"
-                      scope="row"
-                    >
-                      { transaction.status }
-                    </TableCell>
-                    <TableCell
-                      component="th"
-                      scope="row"
-                    >
-                      { transaction.date?.toLocaleString() ?? 'No date provided' }
-                    </TableCell>
-                  </TableRow>
-                )
-              }) }
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Box>
-    </Container>
+
+      { getTabContent() }
+    </Box>
   )
 }
