50fd8c61f5c2802d312c587524ac1d52e3061f43
Clovis de Villars <clovis.devillars@gmail.com>
2021-09-29 13:05:17 +0200

    refactor(Connections): use AutoTabs component instead of Material Tabs

--- renderer/components/getters/Connections.tsx
+++ renderer/components/getters/Connections.tsx
@@ -1,54 +1,30 @@
 import React from 'react'
-import Tabs from '@material-ui/core/Tabs'
-import Tab from '@material-ui/core/Tab'
-import { Box } from '@material-ui/core'
-import MapIcon from '@material-ui/icons/Map'
-import ListIcon from '@material-ui/icons/List'
 import { GetterData } from '@d4data/archive-lib/dist/src/types/standardizer/GetterReturn'
-import { Timeline } from '@material-ui/icons'
+import { List as ListIcon, Map as MapIcon, Timeline } from '@material-ui/icons'
+import { Box } from '@material-ui/core'
 import type { Connection } from '@d4data/archive-lib/dist/src/types/schemas'
 import ConnectionHistory from './LocationComponents/ConnectionHistory'
 import ConnectionsMap from './LocationComponents/ConnectionsMap'
 import Getters from '../../../../d4data-archive-lib/dist/src/types/standardizer/Getters'
 import AutoStatisticPage from '../statistics/AutoStatisticPage'
+import AutoTabs from '../AutoTabs'
 
 export default function Connections({ data }: { data: NonNullable<GetterData<Array<Connection>>> }) {
-  const [value, setValue] = React.useState(0)
-
-  const handleChange = (event: any, newValue: any) => {
-    setValue(newValue)
-  }
-
-  const getTabContent = () => {
-    switch (value) {
-      case 0:
-        return <AutoStatisticPage getter={ Getters.CONNECTIONS }/>
-      case 1:
-        return <ConnectionsMap connections={ data.data }/>
-      case 2:
-        return <ConnectionHistory whereabouts={ data.data }/>
-      default:
-        return undefined
-    }
-  }
-
   return (
-    <Box width={ 1 } display="flex" flexDirection="column" alignItems="center" id="test">
-      <Tabs
-        value={ value }
-        onChange={ handleChange }
-        indicatorColor="primary"
-        textColor="primary"
-        centered
-      >
-        <Tab icon={ <Timeline/> } label="History stat"/>
-        <Tab label="World map" icon={ <MapIcon/> }/>
-        <Tab label="Connection history" icon={ <ListIcon/> }/>
-      </Tabs>
-
-      <Box width={ 1 } flexGrow={ 1 }>
-        { getTabContent() }
-      </Box>
-    </Box>
+    <AutoTabs
+      tabs={ [
+        { label: 'History stat', icon: <Timeline/> },
+        { label: 'World map', icon: <MapIcon/> },
+        { label: 'Connection history', icon: <ListIcon/> },
+      ] }
+      tabsContent={ [
+        <AutoStatisticPage getter={ Getters.CONNECTIONS }/>,
+        <ConnectionsMap connections={ data.data }/>,
+        // Wrap with no overflow container to fix buggy table scroll
+        <Box height={ 1 } overflow="hidden">
+          <ConnectionHistory whereabouts={ data.data }/>
+        </Box>,
+      ] }
+    />
   )
 }
