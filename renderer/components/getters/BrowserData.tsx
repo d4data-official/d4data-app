7093dc89260071fc4e7a437e0133cfc81cb2bad1
Clovis de Villars <clovis.devillars@gmail.com>
2021-10-06 17:13:08 +0200

    refactor(BrowserData): use AutoTabs

--- renderer/components/getters/BrowserData.tsx
+++ renderer/components/getters/BrowserData.tsx
@@ -1,8 +1,6 @@
-import React, { useState } from 'react'
-import { Box, Tab, Tabs } from '@material-ui/core'
+import React from 'react'
+import { Box } from '@material-ui/core'
 import { ColorLens, Extension, History, ListAlt, Settings, Timeline } from '@material-ui/icons'
-import { makeStyles } from '@material-ui/styles'
-import { grey } from '@material-ui/core/colors'
 import type { GetterData } from '@d4data/archive-lib/dist/src/types/standardizer/GetterReturn'
 import type { BrowserData as BrowserDataType } from '@d4data/archive-lib/dist/src/types/schemas'
 import BrowserHistory from './BrowserData/BrowserHistory'
@@ -12,75 +10,29 @@ import BrowserThemes from './BrowserData/BrowserThemes'
 import BrowserSavedForms from './BrowserData/BrowserSavedForms'
 import Getters from '../../../../d4data-archive-lib/dist/src/types/standardizer/Getters'
 import AutoStatisticPage from '../statistics/AutoStatisticPage'
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: `calc(100% + ${ theme.spacing(3) })`,
-    width: '100%',
-    margin: `-${ theme.spacing(3) }`,
-    flexGrow: 1,
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  tabs: {
-    '& .Mui-selected': {
-      background: grey['50'],
-    },
-    minHeight: 72,
-  },
-  tabsRoot: {
-    justifyContent: 'center',
-  },
-  tabsScroller: {
-    flexGrow: 0,
-  },
-}))
+import AutoTabs from '../AutoTabs'
 
 export default function BrowserData({ data }: { data: NonNullable<GetterData<BrowserDataType>> }) {
-  const classes = useStyles()
-  const [currentTab, setCurrentTab] = useState(0)
-
-  const getTabContent = () => {
-    switch (currentTab) {
-      case 0:
-        return <AutoStatisticPage getter={ Getters.BROWSER_DATA }/>
-      case 1:
-        return <BrowserHistory data={ data.data.history }/>
-      case 2:
-        return <BrowserExtensions data={ data.data.extensions }/>
-      case 3:
-        return <BrowserSettings data={ data.data.preferences }/>
-      case 4:
-        return <BrowserThemes data={ data.data.theme }/>
-      case 5:
-        return <BrowserSavedForms data={ data.data.savedForms }/>
-      default:
-        return undefined
-    }
-  }
-
   return (
-    <Box className={ classes.root }>
-      <Box padding={ 4 } paddingBottom={ 0 }>
-        <Tabs
-          classes={ { root: classes.tabsRoot, scroller: classes.tabsScroller } }
-          className={ classes.tabs }
-          value={ currentTab }
-          onChange={ (event, newValue) => setCurrentTab(newValue) }
-          variant="scrollable"
-          indicatorColor="primary"
-          textColor="primary"
-        >
-          <Tab icon={ <Timeline/> } label="History stat"/>
-          <Tab icon={ <History/> } label="History"/>
-          <Tab icon={ <Extension/> } label="Extensions"/>
-          <Tab icon={ <Settings/> } label="Settings"/>
-          <Tab icon={ <ColorLens/> } label="Themes"/>
-          <Tab icon={ <ListAlt/> } label="Forms"/>
-        </Tabs>
-      </Box>
-
-      <Box flexGrow={ 1 } padding={ 0 } overflow="hidden">{ getTabContent() }</Box>
+    <Box height={ 1 } width={ 1 } padding={ 2 } flexGrow={ 1 }>
+      <AutoTabs
+        tabs={ [
+          { label: 'History stat', icon: <Timeline/> },
+          { label: 'History', icon: <History/> },
+          { label: 'Extensions', icon: <Extension/> },
+          { label: 'Settings', icon: <Settings/> },
+          { label: 'Themes', icon: <ColorLens/> },
+          { label: 'Forms', icon: <ListAlt/> },
+        ] }
+        tabsContent={ [
+          <AutoStatisticPage getter={ Getters.BROWSER_DATA }/>,
+          <BrowserHistory data={ data.data.history }/>,
+          <BrowserExtensions data={ data.data.extensions }/>,
+          <BrowserSettings data={ data.data.preferences }/>,
+          <BrowserThemes data={ data.data.theme }/>,
+          <BrowserSavedForms data={ data.data.savedForms }/>,
+        ] }
+      />
     </Box>
   )
 }
