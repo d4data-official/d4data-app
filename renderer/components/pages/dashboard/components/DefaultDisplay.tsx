734945feab94e70ec070aeaccb5c5ac860faa259
Clovis de Villars <clovis.devillars@gmail.com>
2021-10-06 17:31:20 +0200

    feat(DefaultDisplay): add title

--- renderer/components/pages/dashboard/components/DefaultDisplay.tsx
+++ renderer/components/pages/dashboard/components/DefaultDisplay.tsx
@@ -1,28 +1,23 @@
 import { GetterData } from '@d4data/archive-lib/dist/src/types/standardizer/GetterReturn'
-import { makeStyles } from '@material-ui/styles'
 import React from 'react'
 import ReactJson from 'react-json-view-ssr'
+import { Box, Typography } from '@material-ui/core'
 
 export interface Props {
   data: NonNullable<GetterData<any>>
+  // Message indicating that this is a fallback display
 }
 
-const useStyles = makeStyles({
-  container: {
-    width: '100%',
-    marginBottom: '20px',
-  },
-})
-
-export default function DefaultDisplay({ data: { data: src } }: Props) {
-  const classes = useStyles()
+export default function DefaultDisplay({ data }: Props) {
   return (
-    <div className={ classes.container }>
+    <Box height={ 1 } width={ 1 } padding={ 2 }>
+      <Typography variant="h5">Fallback display</Typography>
+      <Typography variant="body2" mb={ 2 }>Custom display for this data type coming soon</Typography>
+
       <ReactJson
         name={ false }
-        src={ src }
-        collapsed
+        src={ data.data }
       />
-    </div>
+    </Box>
   )
 }
