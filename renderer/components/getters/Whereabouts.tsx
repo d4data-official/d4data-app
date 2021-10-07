f3ad3206e912e4f2ae183a6828900c2d85972ba1
Clovis de Villars <clovis.devillars@gmail.com>
2021-10-07 12:24:53 +0200

    style(Whereabouts): fix import order

--- renderer/components/getters/Whereabouts.tsx
+++ renderer/components/getters/Whereabouts.tsx
@@ -4,12 +4,12 @@ import ListIcon from '@material-ui/icons/List'
 import { GetterData } from '@d4data/archive-lib/dist/src/types/standardizer/GetterReturn'
 import { Timeline } from '@material-ui/icons'
 import Getters from '@d4data/archive-lib/dist/src/types/standardizer/Getters'
+import { Box } from '@material-ui/core'
 import type { Whereabout } from '@d4data/archive-lib/dist/src/types/schemas'
 import LocationHistory from './LocationComponents/LocationHistory'
 import WhereaboutsMap from './LocationComponents/WhereaboutsMap'
 import AutoStatisticPage from '../statistics/AutoStatisticPage'
 import AutoTabs from '../AutoTabs'
-import { Box } from '@material-ui/core'
 
 export default function Whereabouts({ data }: { data: NonNullable<GetterData<Array<Whereabout>>> }) {
   return (
