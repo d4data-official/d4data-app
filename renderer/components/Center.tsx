96caff34b7817a7410e0e3a253cec0007d7b6e06
Clovis de Villars <clovis.devillars@gmail.com>
2021-10-08 12:00:00 +0200

    feat(Center): add sx props

--- renderer/components/Center.tsx
+++ renderer/components/Center.tsx
@@ -1,9 +1,14 @@
 import { PropsWithChildren } from 'react'
 import { Box } from '@material-ui/core'
+import { SxProps } from '@material-ui/system'
 
-export default function Center({ children }: PropsWithChildren<{}>) {
+export interface Props {
+  sx?: SxProps
+}
+
+export default function Center({ sx, children }: PropsWithChildren<Props>) {
   return (
-    <Box height={ 1 } width={ 1 } display="flex" alignItems="center" justifyContent="center">
+    <Box height={ 1 } width={ 1 } display="flex" alignItems="center" justifyContent="center" sx={ sx }>
       { children }
     </Box>
   )
