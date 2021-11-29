4d512e40b907a4e0f2c70be600f52eb65888f411
Clovis de Villars <clovis.devillars@gmail.com>
2021-11-29 19:15:32 +0100

    feat(Overview): add translations

--- renderer/components/Overview.tsx
+++ renderer/components/Overview.tsx
@@ -10,39 +10,20 @@ import GoogleIcon from '@mui/icons-material/Google'
 import EventNoteIcon from '@mui/icons-material/EventNote'
 import moment from 'moment'
 import MetadataCard from '@components/pages/dashboard/components/MetaDataCard'
+import { useTranslation } from 'react-i18next'
+import Services from '@d4data/archive-lib/dist/src/types/Services'
 import useArchiveManager from '../hooks/useArchiveManager'
 
-function computeLogoFromServiceName(serviceName: string) {
-  if (serviceName.toUpperCase() === 'FACEBOOK') return (<FacebookIcon/>)
-  if (serviceName.toUpperCase() === 'REDDIT') return (<RedditIcon/>)
-  // if (serviceName.toUpperCase() === 'DISCORD') return (<Reddit/>)
-  if (serviceName.toUpperCase() === 'GOOGLE') return (<GoogleIcon/>)
+function computeLogoFromServiceName(service: Services | undefined) {
+  if (service === Services.FACEBOOK) return (<FacebookIcon/>)
+  if (service === Services.REDDIT) return (<RedditIcon/>)
+  if (service === Services.GOOGLE) return (<GoogleIcon/>)
   return (<UnarchiveIcon/>)
 }
 
-function generateTeamNotes() {
-  return (
-    <div>
-      <h2>A note from the team</h2>
-
-      <Card component="fieldset" style={ { borderColor: 'transparent', padding: 15, paddingBottom: 5 } }>
-        <Grid container>
-          <Grid item>
-            <Typography component="legend">GDPR Score (coming soon)</Typography>
-            <Rating name="read-only" value={ 0 } disabled readOnly/>
-          </Grid>
-        </Grid>
-        <Grid item>
-          <p>This section will help you know more about the services you are using and their data policies.</p>
-          <p>D4Data will set a score for each supported services. More to come during the release!</p>
-        </Grid>
-      </Card>
-
-    </div>
-  )
-}
-
 export default function Overview() {
+  const { t } = useTranslation(['common', 'overview'])
+
   const [archiveMetadata, setArchiveMetadata] = useState<ArchiveMetaData | undefined>()
   const { getArchiveMetadata, restoredArchive } = useArchiveManager()
 
@@ -57,15 +38,13 @@ export default function Overview() {
 
   return (
     <Container>
-      <h2>About your archive</h2>
+      <h2>{ t('overview:about') }</h2>
       <Grid container spacing={ 4 }>
         <Grid item xs={ 4 }>
           <MetadataCard
-            icon={ archiveMetadata?.service
-              ? computeLogoFromServiceName(archiveMetadata?.service)
-              : 'Loading...' }
+            icon={ computeLogoFromServiceName(archiveMetadata?.service) }
             metadata={ archiveMetadata?.service }
-            subtitle="Service"
+            subtitle={ t('common:service') }
           />
 
         </Grid>
@@ -75,7 +54,7 @@ export default function Overview() {
             <MetadataCard
               icon={ <UnarchiveIcon/> }
               metadata={ Filesize(archiveMetadata?.size) }
-              subtitle="Archive size"
+              subtitle={ t('overview:size') }
             />
           </Grid>
         ) }
@@ -85,15 +64,33 @@ export default function Overview() {
             <MetadataCard
               icon={ <EventNoteIcon/> }
               metadata={ archiveCreationDateDuration.humanize() }
-              subtitle="Archive age"
+              subtitle={ t('overview:age') }
             />
           </Grid>
         ) }
       </Grid>
+
       <br/>
-      <div>
-        { archiveMetadata?.service && generateTeamNotes() }
-      </div>
+
+      { archiveMetadata?.service && (
+        <div>
+          <h2>{ t('overview:note') }</h2>
+
+          <Card component="fieldset" style={ { borderColor: 'transparent', padding: 15, paddingBottom: 5 } }>
+            <Grid container>
+              <Grid item>
+                <Typography component="legend">{ t('overview:score') }</Typography>
+                <Rating name="read-only" value={ 0 } disabled readOnly/>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <p>{ t('overview:noteDescription') }</p>
+              <p>{ t('overview:scoreDescription') }</p>
+            </Grid>
+          </Card>
+
+        </div>
+      ) }
     </Container>
   )
 }
