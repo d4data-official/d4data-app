923a77f72e29957f129793e9056fe048b5a261c3
Clovis de Villars <clovis.devillars@gmail.com>
2021-11-29 16:04:47 +0100

    feat(Loading): add missing translations

--- renderer/components/pages/dashboard/components/Loading.tsx
+++ renderer/components/pages/dashboard/components/Loading.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Box, CircularProgress, Fade, Stack, Typography } from '@mui/material'
+import { useTranslation } from 'react-i18next'
 import Center from '../../../Center'
 
 interface Props {
@@ -8,22 +9,22 @@ interface Props {
 }
 
 export default function Loading({ title, description }: Props) {
+  const { t } = useTranslation('loadingComponent')
+
   return (
     <Center>
       <Stack alignItems="center" spacing={ 1 }>
         <Fade in timeout={ 1000 }>
           <Stack alignItems="center" spacing={ 1 }>
             <CircularProgress variant="indeterminate" size={ 50 }/>
-            <Typography variant="h4" fontWeight={ 100 }>{ title ?? 'Loading' }</Typography>
+            <Typography variant="h4" fontWeight={ 100 }>{ title ?? t('title') }</Typography>
           </Stack>
         </Fade>
 
         { description ?? (
           <Box overflow="hidden">
             <Fade in timeout={ 2000 }>
-              <Typography variant="body1" color="gray">It seems that this service collected some data about you in this
-                category...
-              </Typography>
+              <Typography variant="body1" color="gray">{ t('description') }</Typography>
             </Fade>
           </Box>
         ) }
